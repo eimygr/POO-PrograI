e6d2b74db8a6b5d2fdfeb53d4669b7b4166080d6
eimygr <eimygr@gmail.com>
2017-04-05 16:53:22 -0600

    Atributos clase Articulo y Revista

---
+++ src/Articulo.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Eimy on 05/04/2017.
+ */
+public class Articulo {
+    private String nombre;
+    private int año;
+    private boolean tipo;
+
+}
--- src/Revista.java
+++ src/Revista.java
@@ -2,4 +2,10 @@
  * Created by Eimy on 05/04/2017.
  */
 public class Revista {
+    private static String id;
+    private Estado estado;
+    private int num;
+    private double costo;
+    private String categoria;
+    
 }
