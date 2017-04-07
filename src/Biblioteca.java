a2e78327f80147205ff98197275450b077540e04
danialvaradob <danielalvarado@Daniels-MacBook-Pro.local>
2017-04-07 17:19:12 -0600

    Adding verificar correo

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,4 +1,7 @@
 import java.util.Date;
+package biblioteca;
+
+
 
 public class Biblioteca {
     private String nombre;
@@ -6,46 +9,37 @@ public class Biblioteca {
     private int telefono;
     private String bibliotecologo;
     private Date fechaActual;
-    public class Biblioteca {
-        
-        /**
-         * @param args the command line arguments
-         */
-        
 
-        private Libro[] listaLibros;
-        private Revista[] listaRevistas;
-        private Cliente[] listaClientes;
-        
-        
-        private boolean validarTelefono(int _numTel) {
-            String telefono = Integer.toString(_numTel);
-            return telefono.length() == 8;
-        }
-        
-        private boolean validarCorreo(String _correoE) {
-            
-            if (_correoE.length() == 0 )
-                return false;
-            
-            if (_correoE.charAt(0) == '@')
-                return false;
-            
-            for (int i = 0; i < _correoE.length(); i++){
-                char letra = _correoE.charAt(i);
-                
-                if (letra == '@'){
-                    if (_correoE.charAt(i + 1) == ' ') {
-                        return false ;
-                    }   
-                }
+    private Libro[] listaLibros;
+    private Revista[] listaRevistas;
+    private Cliente[] listaClientes;
+
+
+    private boolean validarTelefono(int _numTel) {
+        String telefono = Integer.toString(_numTel);
+        return telefono.length() == 8;
+    }
+
+    private boolean validarCorreo(String _correoE) {
+
+        if (_correoE.length() == 0 )
+            return false;
+
+        if (_correoE.charAt(0) == '@')
+            return false;
+
+        for (int i = 0; i < _correoE.length(); i++){
+            char letra = _correoE.charAt(i);
+
+            if (letra == '@'){
+                if (_correoE.charAt(i + 1) == ' ') {
+                    return false ;
+                }   
             }
-            return true;
         }
-        
-        
-    
-    
+        return true;
+    }
+
 }
 
-public void validarDatos(String pNombre, String pCorreo, String pCedula)
+//public void validarDatos(String pNombre, String pCorreo, String pCedula)
