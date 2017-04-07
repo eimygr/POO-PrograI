6b2e2db1d9e10d8091cfdac3d75da0d8a73314cc
danialvaradob <danielalvarado@Daniels-MacBook-Pro.local>
2017-04-07 11:27:56 -0600

    Adding verificar correo

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -6,7 +6,46 @@ public class Biblioteca {
     private int telefono;
     private String bibliotecologo;
     private Date fechaActual;
+    public class Biblioteca {
+        
+        /**
+         * @param args the command line arguments
+         */
+        
 
+        private Libro[] listaLibros;
+        private Revista[] listaRevistas;
+        private Cliente[] listaClientes;
+        
+        
+        private boolean validarTelefono(int _numTel) {
+            String telefono = Integer.toString(_numTel);
+            return telefono.length() == 8;
+        }
+        
+        private boolean validarCorreo(String _correoE) {
+            
+            if (_correoE.length() == 0 )
+                return false;
+            
+            if (_correoE.charAt(0) == '@')
+                return false;
+            
+            for (int i = 0; i < _correoE.length(); i++){
+                char letra = _correoE.charAt(i);
+                
+                if (letra == '@'){
+                    if (_correoE.charAt(i + 1) == ' ') {
+                        return false ;
+                    }   
+                }
+            }
+            return true;
+        }
+        
+        
+    
+    
 }
 
 public void validarDatos(String pNombre, String pCorreo, String pCedula)
