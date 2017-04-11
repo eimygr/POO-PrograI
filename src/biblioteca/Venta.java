9e6997562895e29e5c115026c5de50c4ee671edb
eimygr <eimygr@gmail.com>
2017-04-10 18:28:22 -0600

    Constructor Venta

--- src/biblioteca/Venta.java
+++ src/biblioteca/Venta.java
@@ -1,9 +1,19 @@
 package biblioteca;
 
 import java.util.Date;
+import java.util.Vector;
 
 public class Venta {
     private Date fecha;
-    private Revista revista;
+    private Vector<Revista> listaCompras =  new Vector<Revista>();
+    private Cliente cliente;
+
+    Venta(Date _fecha, Vector<Revista> _listaCompras, Cliente _cliente){
+        fecha = _fecha;
+        listaCompras = _listaCompras;
+        cliente = _cliente;
+    }
 
 }
+
+
