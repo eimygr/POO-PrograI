9bcd8050993af3fde36e89bc1f720861d9a875e8
rrsegnini <roberto.t.x@hotmail.com>
2017-04-13 12:38:10 -0600

    Pruebas para la lectura de archivos de Excel
    Estos haciendo un despiche ahí

--- src/biblioteca/Biblioteca.java
+++ src/biblioteca/Biblioteca.java
@@ -1,8 +1,22 @@
 package biblioteca;
+import java.io.File;
 import java.util.Date;
 import java.util.*;
 import static java.lang.System.*;
 
+//Librerias y archivos para leer de Excel
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+
+
+import org.apache.poi.ss.usermodel.Cell;
+
+import org.apache.poi.ss.usermodel.Row;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+//End Librerias y archivos para leer de Excel
 
 public class Biblioteca {
     private String nombre;
@@ -21,7 +35,8 @@ public class Biblioteca {
     private Vector<Venta> listaVentas =  new Vector<Venta>();
     private Vector<Prestamo> listaPrestamo =  new Vector<Prestamo>();
     
-    
+    public Biblioteca(){
+    }
     public Biblioteca(String _nombre, String _ubicacion, int _telefono
             , String _bibliotecologo, Date _fechaActual, int _diasPrestamoLibro,
             int _diasPrestamoRevista) {
@@ -345,10 +360,67 @@ public class Biblioteca {
     
     }
     
-    
-    
-    
-    
+    public void CrearExcel() 
+    {
+        //Blank workbook
+        XSSFWorkbook workbook = new XSSFWorkbook(); 
+         
+        //Create a blank sheet
+        XSSFSheet sheet = workbook.createSheet("Employee Data");
+          
+        //This data needs to be written (Object[])
+        Map<String, Object[]> data = new TreeMap<String, Object[]>();
+        data.put("1", new Object[] {"ID", "NAME", "LASTNAME"});
+        data.put("2", new Object[] {1, "Amit", "Shukla"});
+        data.put("3", new Object[] {2, "Lokesh", "Gupta"});
+        data.put("4", new Object[] {3, "John", "Adwards"});
+        data.put("5", new Object[] {4, "Brian", "Schultz"});
+          
+        //Iterate over data and write to sheet
+        Set<String> keyset = data.keySet();
+        int rownum = 0;
+        for (String key : keyset)
+        {
+            Row row = sheet.createRow(rownum++);
+            Object [] objArr = data.get(key);
+            int cellnum = 0;
+            for (Object obj : objArr)
+            {
+               Cell cell = row.createCell(cellnum++);
+               if(obj instanceof String)
+                    cell.setCellValue((String)obj);
+                else if(obj instanceof Integer)
+                    cell.setCellValue((Integer)obj);
+            }
+        }
+        try
+        {
+            //Write the workbook in file system
+            FileOutputStream out = new FileOutputStream(new File("howtodoinjava_demo.xlsx"));
+            workbook.write(out);
+            out.close();
+            System.out.println("howtodoinjava_demo.xlsx written successfully on disk.");
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
 }
+
+
+/* Bibliografia
+https://poi.apache.org/apidocs/org/apache/poi/ss/usermodel/Row.html
+https://docs.oracle.com/javase/7/docs/api/java/io/File.html
+
+Para descargar xmlbeans: http://www.java2s.com/Code/Jar/x/Downloadxmlbeans230jar.htm
+https://mvnrepository.com/artifact/org.apache.commons/commons-collections4/4.0
+*/
+    
+    
+    
+    
+    
+//}
 
 //public void validarDatos(String pNombre, String pCorreo, String pCedula)
--- src/biblioteca/Main.java
+++ src/biblioteca/Main.java
@@ -1,8 +1,18 @@
 package biblioteca;
 
 public class Main {
-
-    public static void main(String[] args) {Prestamo.EnviarCorreo("rojassegniniroberto@gmail.com", "Sergio, playo");
+    
+    public static void main(String[] args) {
+        //Prestamo.EnviarCorreo("eimygr@gmail.com", "Eimy, lesbiana");
+        String[] archivo = new String[1];
+        archivo[0] = "Libros";
+        
+        //Biblioteca.testCreateSampleSheet("Libros");
+        //Biblioteca.registrarExcel(archivo);
+        Biblioteca a = new Biblioteca();
+        
+        a.CrearExcel();
+        
     }
 
 
