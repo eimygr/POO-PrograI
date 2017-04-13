25fc66552ec3caed9c0f071030bcbb5aeda6edff
rrsegnini <roberto.t.x@hotmail.com>
2017-04-13 15:57:49 -0600

    Update Venta

--- src/biblioteca/Venta.java
+++ src/biblioteca/Venta.java
@@ -7,11 +7,13 @@ public class Venta {
     private Date fecha;
     private Vector<Revista> listaCompras =  new Vector<Revista>();
     private Cliente cliente;
+    private String NombreArticulo;
 
-    Venta(Date _fecha, Vector<Revista> _listaCompras, Cliente _cliente){
+    Venta(Date _fecha, Vector<Revista> _listaCompras, Cliente _cliente,String _nombre){
         fecha = _fecha;
         listaCompras = _listaCompras;
         cliente = _cliente;
+        this.NombreArticulo = _nombre; 
     }
 
 }
